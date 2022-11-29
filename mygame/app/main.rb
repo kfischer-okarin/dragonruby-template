4b65a43a1b16776f425e123e732cf7b21990e582
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-11-29 20:52:43 +0900

    Fix style

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,4 +1,4 @@
-def tick args
+def tick(args)
   args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
   args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
   args.outputs.labels  << [640, 420, 'Join the Discord! https://discord.dragonruby.org', 5, 1]
